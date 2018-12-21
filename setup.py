31f3017de748f8d81f8b4643ce5e6721f7ae487c
DenisMoshenskiy <Denis.Moshenskiy@allianz.ru>
2018-12-21 18:06:23 +0300

    readme updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='MaLeMBa',
-    version='0.1.3',
+    version='0.1.4',
     author="Denis Moshensky",
     author_email="loven7doo@gmail.com",  
     description="Machine Learning Models Base",
