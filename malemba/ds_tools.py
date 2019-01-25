95414ab8ea0215148ddd0d0eda565f9fa0c5a524
dmm <moshenskydenis@gmail.com>
2019-01-25 19:02:51 +0300

    aggregate method upgraded

--- malemba/ds_tools.py
+++ malemba/ds_tools.py
@@ -3,9 +3,9 @@ class ArrayHandler(object):
     def __init__(self, group_lims=None):
         self.group_lims = group_lims
 
-    def aggregate(self, array, aggr_level, _top_level=True):
+    def aggregate(self, array, aggr_level):
         if aggr_level < 1:
-            return array
+            yield array
         if self.group_lims is None:
             self.group_lims = [list() for k in range(aggr_level)]
             i = -1
@@ -14,31 +14,18 @@ class ArrayHandler(object):
                 i = self.group_lims[aggr_level - 1][-1]
             except IndexError:
                 i = -1
-        aggr_array = list()
         if aggr_level == 1:
             for array_elm in array:
-                if _top_level:
-                    for elm in array_elm:
-                        yield elm
-                else:
-                    aggr_array.extend(array_elm)
+                for elm in array_elm:
+                    yield elm
                 i += len(array_elm)
                 self.group_lims[0].append(i)
         else:
             for array_elm in array:
-                aggr_array_part = self.aggregate(array=array_elm,
-                                                 aggr_level=aggr_level-1,
-                                                 _top_level=False)
-                if _top_level:
-                    for elm in aggr_array_part:
-                        yield elm
-                else:
-                    aggr_array.extend(aggr_array_part)
+                for elm in self.aggregate(array=array_elm, aggr_level=aggr_level-1):
+                    yield elm
                 i += len(array_elm)
                 self.group_lims[aggr_level-1].append(i)
-
-        if not _top_level:
-            return aggr_array
 
 
 def group_array(aggr_array, group_lims):
