2db685de998388175f86f861f271078ccd2726f0
dmm <moshenskydenis@gmail.com>
2019-01-25 18:01:54 +0300

    created ArrayHandler

--- malemba/ds_tools.py
+++ malemba/ds_tools.py
@@ -1,26 +1,44 @@
-def aggregate_array(array, aggr_level, group_lims=None):
-    if aggr_level < 1:
-        return array
-    if group_lims is None:
-        group_lims = [list() for k in range(aggr_level)]
-        i = -1
-    else:
-        try:
-            i = group_lims[aggr_level - 1][-1]
-        except IndexError:
+class ArrayHandler(object):
+
+    def __init__(self, group_lims=None):
+        self.group_lims = group_lims
+
+    def aggregate(self, array, aggr_level, _top_level=True):
+        if aggr_level < 1:
+            return array
+        if self.group_lims is None:
+            self.group_lims = [list() for k in range(aggr_level)]
             i = -1
-    aggr_array = list()
-    if aggr_level == 1:
-        for array_elm in array:
-            aggr_array.extend(array_elm)
-            i += len(array_elm)
-            group_lims[0].append(i)
-    else:
-        for array_elm in array:
-            aggr_array.extend(aggregate_array(array=array_elm, aggr_level=aggr_level-1, group_lims=group_lims)[0])
-            i += len(array_elm)
-            group_lims[aggr_level-1].append(i)
-    return aggr_array, group_lims
+        else:
+            try:
+                i = self.group_lims[aggr_level - 1][-1]
+            except IndexError:
+                i = -1
+        aggr_array = list()
+        if aggr_level == 1:
+            for array_elm in array:
+                if _top_level:
+                    for elm in array_elm:
+                        yield elm
+                else:
+                    aggr_array.extend(array_elm)
+                i += len(array_elm)
+                self.group_lims[0].append(i)
+        else:
+            for array_elm in array:
+                aggr_array_part = self.aggregate(array=array_elm,
+                                                 aggr_level=aggr_level-1,
+                                                 _top_level=False)
+                if _top_level:
+                    for elm in aggr_array_part:
+                        yield elm
+                else:
+                    aggr_array.extend(aggr_array_part)
+                i += len(array_elm)
+                self.group_lims[aggr_level-1].append(i)
+
+        if not _top_level:
+            return aggr_array
 
 
 def group_array(aggr_array, group_lims):
