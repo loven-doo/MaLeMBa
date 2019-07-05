97cf585270d960768f2a448342598ede41707c9a
Denis <loven7doo@gmail.com>
2019-07-05 15:43:52 +0300

    ModelScheme added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='MaLeMBa',
-    version='0.4.9',
+    version='0.4.10',
     author="Denis Moshensky",
     author_email="loven7doo@gmail.com",  
     description="Machine Learning Models Base",
@@ -24,6 +24,7 @@ setup(
         'pandas >= 0.23.4',
         'scipy >= 1.1.0',
         'shared-ndarray >= 1.1.1',
+        'jsondler >= 0.0.1',
     ],
     entry_points={
         'console_scripts': []
