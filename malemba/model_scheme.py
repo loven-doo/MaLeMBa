19d8537325d5cf379ffd92e4be11dcbce65ae361
Denis <loven7doo@gmail.com>
2019-07-18 00:52:51 +0300

    params_dict key added to ModelScheme

--- malemba/model_scheme.py
+++ malemba/model_scheme.py
@@ -8,18 +8,23 @@ class ModelScheme(JsonEntry):
 
     method_name_key = "method_name"
     model_path_key = "model_path"
+    params_dict_key = "params_dict"
     basic_models_key = "basic_models"
 
     method_name_0 = None
     model_path_0 = None
+    params_dict_0 = dict()
     basic_models_0 = dict()
 
-    def __init__(self, method_name=method_name_0, model_path=model_path_0, basic_models=None):
+    def __init__(self, method_name=method_name_0, model_path=model_path_0, params_dict=None, basic_models=None):
         if basic_models is None:
             basic_models = self.basic_models_0
+        if params_dict is None:
+            params_dict = self.params_dict_0
 
         self.method_name = method_name
         self.model_path = model_path
+        self.params_dict = params_dict
         self.basic_models = basic_models
 
     @classmethod
@@ -27,6 +32,7 @@ class ModelScheme(JsonEntry):
         attr_scheme={
             'method_name': (cls.method_name_key,),
             'model_path': (cls.model_path_key,),
+            'params_dict': (cls.params_dict_key,),
             'basic_models': (cls.basic_models_key,),
         }
         return attr_scheme
